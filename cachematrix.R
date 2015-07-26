60587efff0b1d5611b2f3d18d382b2ba3ea392e7
Mario Gordillo <mfgordillom@hotmail.com>
2015-07-26 12:20:38 -0600

    Programming assignment 2
    Mario Gordillo

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Mario Gordillo
+## GUATEMALA
+## Examinig the code, I found the answer by substituting the mean function
+## by the solve function
 
-## Write a short comment describing this function
+## This function creates an "object" of the type matrix() and stores it in
+## scope/memory
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	  m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setmean <- function(mean) m <<- mean
+        getmean <- function() m
+        list(set = set, get = get,
+             setmean = setmean,
+             getmean = getmean)
 }
 
 
-## Write a short comment describing this function
+## This function uses the "object" created by the previous function and
+## "invert it" with the solve() function.  If the object exists, it only
+## takes the value (to avoid re-processing).  If not, then, it does the 
+## processing
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+	  m <- x$getmean()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setmean(m)
+        m	
 }
